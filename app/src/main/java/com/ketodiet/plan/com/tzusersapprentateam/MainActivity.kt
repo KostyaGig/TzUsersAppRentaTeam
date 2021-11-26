4aefc7e0ed9a2addd4dd465a85cbf55ebff0c11b
kostyagig <k.gig@list.ru>
2021-11-26 18:23:07 +0300

    Initial commit

---
+++ app/src/main/java/com/ketodiet/plan/com/tzusersapprentateam/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ketodiet.plan.com.tzusersapprentateam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
